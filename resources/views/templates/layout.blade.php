6c999d78e77066dc97795f47ae4056ea4723a7bc
tleopardt <59083536+tleopardt@users.noreply.github.com>
2021-10-24 14:26:48 +0700

    s

--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -6,7 +6,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{ asset ('css/app.css')}}"></link>
     <link rel="stylesheet" href="{{ asset ('splide/dist/css/splide.min.css')}}"></link>
-    <!-- <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"/> -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
     <title>@yield('title')</title>
 </head>
