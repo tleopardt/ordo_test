4d59a7112eda7d8f96c0e3826c33e256f67b6683
tleopardt <59083536+tleopardt@users.noreply.github.com>
2021-10-25 08:57:18 +0700

    s

--- resources/views/templates/layout.blade.php
+++ resources/views/templates/layout.blade.php
@@ -57,7 +57,6 @@
 <!-- Slider JS -->
 <script src="{{ asset ('splide/dist/js/splide.min.js')}}"></script>
 <script>
-    // $('body').on('load').css("opacity", "1")
     // slide script
     if($('.splide').length !== 0) {
         var sliderFunction = new Splide('.splide' ,{
